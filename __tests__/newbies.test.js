b08bc50592ae3d1c9fc144074183e8fe0e4c3387
Adam Robson <adamray312@gmail.com>
2022-11-14 10:23:16 -0800

    fix bugs in first two routes for newbies

--- __tests__/newbies.test.js
+++ __tests__/newbies.test.js
@@ -11,32 +11,30 @@ describe('newbies routes', () => {
   test('GET /newbies/:id returns a newbie detail', async () => {
     const res = await request(app).get('/newbies');
     const singleNewbie = await res.body.find((newbie) => (newbie.id = '1'));
-    // expect(singleNewbie).toMatchObject({
-    //   first_name: 'Darcie',
-    //   last_name: 'Jorden',
-    //   email_address: 'djordeny@sakura.ne.jp',
-    //   gender: 'Female',
-    //   ip_address: '80.6.214.168'
-    // });
+    expect(singleNewbie).toMatchObject({
+      first_name: 'Darcie',
+      last_name: 'Jorden',
+      email_address: 'djordeny@sakura.ne.jp',
+      gender: 'Female',
+      ip_address: '80.6.214.168'
+    });
     expect(singleNewbie).toMatchInlineSnapshot(`
       Object {
-        "dob": "19600105",
-        "email_address": "tofficer@amazon.de",
-        "first_name": "Teddy",
+        "email_address": "djordeny@sakura.ne.jp",
+        "first_name": "Darcie",
         "gender": "Female",
         "id": "1",
-        "last_name": "Officer",
+        "ip_address": "80.6.214.168",
+        "last_name": "Jorden",
       }
     `);
   });
-  
+
   test('GET /newbies returns a list of newbies', async () => {
     const res = await request(app).get('/newbies');
-    expect(res.body.length).toEqual(10);
+    expect(res.body.length).toEqual(11);
     expect(res.statusCode).toEqual(200);
   });
-
-
 
   afterAll(() => {
     return setup(pool);
