b062c00e0031da99af388708be3573cd35314f81
Adam Robson <adamray312@gmail.com>
2022-11-13 21:38:34 -0800

    adds test controller and model for returning a single pet by id

--- __tests__/pets.test.js
+++ __tests__/pets.test.js
@@ -14,6 +14,18 @@ describe('pets routes', () => {
     expect(res.statusCode).toEqual(200);
   });
 
+  test('GET /pets/:id returns a pet detail', async () => {
+    const res = await request(app).get('/pets');
+    const singleRow = await res.body.find((pet) => (pet.id = '4'));
+    expect(singleRow).toMatchObject({
+      first_name: 'Mayer',
+      last_name: 'Fitzsymons',
+      kind: 'goldfish',
+      variety: 'shubunkin',
+      nickname: 'jeffe'
+    });
+  });
+
   afterAll(() => {
     return setup(pool);
   });
