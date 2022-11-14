3cd4c29f52ca1fd2833f26c2ef69e39a22431342
Adam Robson <adamray312@gmail.com>
2022-11-13 19:22:27 -0800

    build out table for resource  no. 2

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,4 +1,5 @@
 DROP TABLE IF EXISTS associates CASCADE;
+DROP TABLE IF EXISTS candidates CASCADE;
 
 CREATE TABLE associates (
   id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -7,6 +8,15 @@ CREATE TABLE associates (
   EMAIL_ADDRESS VARCHAR NOT NULL,
   GENDER VARCHAR NOT NULL,
   IP_ADDRESS VARCHAR NOT NULL
+  );
+
+CREATE TABLE candidates (
+  id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  FIRST_NAME VARCHAR NOT NULL,
+  LAST_NAME VARCHAR NOT NULL,
+  EMAIL_ADDRESS VARCHAR NOT NULL,
+  GENDER VARCHAR NOT NULL,
+  DOB DATE NOT NULL
   );
 
   INSERT INTO associates (
@@ -27,3 +37,17 @@ CREATE TABLE associates (
   ('Dori',	'Mattis',	'dmattisc@miibeian.gov.cn',	'Female',	'171.160.95.246'),
   ('Danell',	'Road',	'droadd@issuu.com',	'Female',	'101.149.14.177'),
   ('Venita',	'Haythorn',	'vhaythorne@symantec.com',	'Female', '195.238.129.222');
+
+INSERT INTO candidates (
+  FIRST_NAME, LAST_NAME, EMAIL_ADDRESS, GENDER, DOB)
+  VALUES
+('Teddy',	'Officer', 'tofficerf@amazon.de',	'Female', '1960-01-05'),
+('Annabella',	'Rowbottom',	'arowbottomg@cnet.com',	'Female',	'1983-02-05'),
+('Bendite',	'Pocknell',	'bpocknellh@hhs.gov',	'Bigender',	'1977-08-31'),
+('Estell',	'Sproston',	'esprostoni@nytimes.com',	'Agnostic', '1967-12-12'),
+('Karon',	'Conichie',	'kconichiej@alibaba.com',	'Female', '1988-11-19'),
+('Dion',	'Trusty',	'dtrustyk@globo.com',	'Male',	'1983-05-05'),
+('Corina',	'Andreotti',	'candreottil@scribd.com',	'Bigender', '1979-09-11'),
+('Ileana',	'Deniskevich',	'ideniskevichm@ihg.com',	'Female', '1955-04-12'),
+('Corella',	'Ambrosi',	'cambrosin@free.fr',	'Bigender', '1988-06-30'),
+('Urbano',	'O&apos;Reagan',	'uoreagano@phoca.cz',	'Confused & OK with that', '1984-10-16');
