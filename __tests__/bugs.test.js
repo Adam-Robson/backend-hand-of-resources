e2988385b1f231e18f678ad5d8af67e828cbaa4e
Adam Robson <adamray312@gmail.com>
2022-11-14 11:33:53 -0800

    create POST route and build it out for inserting a record in the bugs table

--- __tests__/bugs.test.js
+++ __tests__/bugs.test.js
@@ -14,7 +14,6 @@ describe('bugs route', () => {
     expect(res.statusCode).toEqual(200);
   });
 
-
   test('GET /bugs/:id route to return an bug detail', async () => {
     const res = await request(app).get('/bugs');
     const singleBug = await res.body.find((bug) => (bug.id = '1'));
@@ -22,6 +21,26 @@ describe('bugs route', () => {
       first_name: 'Benjamin',
       last_name: 'Pallet',
     });
+  });
+
+  test('POST /bugs creates new bug in the database', async () => {
+    const newBug = {
+      first_name: 'B',
+      last_name: 'Honey',
+      kind: 'bee',
+      age: 7,
+    };
+    const res = await request(app).post('/bugs').send(newBug);
+    expect(res.statusCode).toBe(200);
+    expect(res.body).toMatchInlineSnapshot(`
+      Object {
+        "age": 7,
+        "first_name": "B",
+        "id": 6,
+        "kind": "bee",
+        "last_name": "Honey",
+      }
+    `);
   });
 
   afterAll(() => {
